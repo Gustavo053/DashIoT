37f4316d5c0eb54440923ae32d6fca0cb8ec3ac0
Gustavo <gu-gu-pf@hotmail.com>
2020-12-15 22:25:03 -0300

    configured persistence

--- backend/database/persistence.js
+++ backend/database/persistence.js
@@ -3,26 +3,28 @@ const mqtt = require('mqtt');
 const fs = require('fs');
 const path = require('path');
 
-const client = mqtt.connect('mqtt://localhost:1883');
+let client;
 
-//Se conecta e dá um subscribe no broker 'data'
-client.on('connect', () => {
-    client.subscribe('data', (err) => {
-        if (err) {
-            alert('error: ' + err);
-        }
-    });
-});
-
-let i = 0
 ipcMain.on('data-persistence', (event, data) => {
-    //recebe os dados publicados no broker 'data' e renderiza-os no front-end
+    console.log(data);
+
+    if (data == 'start') {
+        client = mqtt.connect('mqtt://localhost:1883');
+
+        //Se conecta e dá um subscribe no broker 'data'
+        client.on('connect', () => {
+            client.subscribe('data', (err) => {
+                if (err) {
+                    alert('error: ' + err);
+                }
+            });
+        });
+    } else {
+        client.end();
+        return
+    }
+
     client.on('message', (topic, message) => {
-        if (data == 'stop') {
-            return;
-        } else {
-            // fs.appendFileSync(path.join(__dirname, 'datas.csv'), message.toString() + '\n');
-            console.log('estou sendo chamado', i++);
-        }
+        fs.appendFileSync(path.join(__dirname, 'datas.csv'), message.toString() + '\n');
     });
 });
